a3e09e355108ffe677e599e1d3e67e1621fbe153
jorgedaktika <jorge@daktika.com>
2023-01-22 00:38:47 +0100

    with chrome

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -7,23 +7,28 @@ namespace TestProject1
     public class Tests
     {
 
-        //public IWebDriver Driver;
+        public IWebDriver driver;
 
         [SetUp]
         public void Setup()
         {
-            //Driver = new ChromeDriver();
+            driver = new ChromeDriver();
         }
 
         [Test]
         public void Test1()
         {
-            //Driver.Navigate().GoToUrl("https://daktika.com/");
-            //Driver.FindElement(By.Id("btn-decline-cookies")).Click();
+            driver.Navigate().GoToUrl("https://daktika.com/");
+            driver.FindElement(By.Id("btn-decline-cookies")).Click();
+            Assert.That(driver.FindElement(By.CssSelector("h2")).Text, Is.EqualTo("¿Quieres que tus alumnos de programación aprendan más, mejor y más rápido?"));
+            var elements = driver.FindElements(By.CssSelector("h2"));
+            Assert.True(elements.Count > 0);
+            driver.Close();
+
+
             //Driver.FindElement(By.CssSelector(".nav-item:nth-child(2) img")).Click();
             //Assert.That(Driver.FindElement(By.CssSelector("h2")).Text, Is.EqualTo("Do you want your programming students to learn more, better and faster?"));
             //Driver.Close();
-            Assert.Pass();
         }
     }
 }
